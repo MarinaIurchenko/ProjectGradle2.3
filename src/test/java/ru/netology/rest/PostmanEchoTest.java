a79d02c79b00c812b43810aa7fc2f07f2d550dbe
Marina Iurchenko <lamanch1203@gmail.com>
2023-06-21 13:12:53 +0300

    initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+package ru.netology.rest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("I'm stupid")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("I'm stopped"));
+    }
+}
+
